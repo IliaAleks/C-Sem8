afb3760285b308ece0fe4fa599239102ff60ceb4
IliaJob <av98060@gmail.com>
2023-01-11 17:56:21 +0300

    Task 54

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Task 54
+Console.WriteLine("Task 54");
+int [,] createMassiv()
+{
+    Console.WriteLine("Исходный двухмерный массив:");
+    Random rnNum= new Random();
+    int [,] newMas=new int [5, 5];
+        for (int i=0; i<5; i++)
+        {
+            for (int j=0; j<5; j++)
+            {
+                newMas[i,j]=rnNum.Next(0, 100);
+                Console.Write($"{newMas[i,j]} ");
+            }
+            Console.WriteLine();
+        }
+        return newMas;
+}
+
+int [,] sortMas(int [,] masin)
+{
+    for (int i=0; i<masin.GetLength(0); i++)
+    {
+        for (int j=0; j<masin.GetLength(1); j++)
+        {
+            // if (masin[i,j+1]>masin[i,j])
+            // {
+            //     int numb =  masin[i,j];
+            //    masin[i,j]= masin[i,j+1];
+            //     masin[i,j+1]=numb;
+            //     j=0;
+            // }
+        }
+    }
+    return masin;
+}
+
+int [,] masivUse=createMassiv();
+sortMas(masivUse);
