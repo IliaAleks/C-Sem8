2f9fb058ffc5b4b322af29f09320c46f16b2573f
Илья Алексеев <av98060@gmail.com>
2023-01-11 22:48:32 +0300

    Task 56

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿//Task 54
-Console.WriteLine("Task 54");
 int [,] createMassiv()
 {
     Console.WriteLine("Исходный двухмерный массив:");
@@ -21,19 +20,106 @@ int [,] sortMas(int [,] masin)
 {
     for (int i=0; i<masin.GetLength(0); i++)
     {
-        for (int j=0; j<masin.GetLength(1); j++)
+        for (int j=0; j<masin.GetLength(1)-1; j++)
         {
-            // if (masin[i,j+1]>masin[i,j])
-            // {
-            //     int numb =  masin[i,j];
-            //    masin[i,j]= masin[i,j+1];
-            //     masin[i,j+1]=numb;
-            //     j=0;
-            // }
+            if (masin[i,j+1]>masin[i,j])
+            {
+                int numb =  masin[i,j];
+               masin[i,j]= masin[i,j+1];
+                masin[i,j+1]=numb;
+                j=0;
+            }
         }
     }
     return masin;
 }
 
-int [,] masivUse=createMassiv();
-sortMas(masivUse);
+// Console.WriteLine("Task 54");
+// int [,] masivUse=createMassiv();
+// int [,] sortMasWork=sortMas(masivUse);
+// Console.WriteLine("Отсортированный массив: ");
+// for (int i=0; i<sortMasWork.GetLength(0); i++)
+// {
+//     for (int j=0; j<sortMasWork.GetLength(1); j++)
+//     {
+//     Console.Write($"{sortMasWork[i,j]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+//Task 56
+Console.WriteLine("Task 56");
+int [,] mas56=createMassiv();
+void strMin(int [,] masMin)
+{
+    int str=0;
+    int numStr=0;
+    for (int i=0; i<masMin.GetLength(0); i++)
+    {
+        int minSum=0;
+        for (int j=0; j<masMin.GetLength(1); j++)
+        {
+           minSum=minSum+masMin[i,j];
+        }
+        if (i==0)
+        {
+            str=minSum;
+        } else
+        {
+            if (minSum<str)
+            {
+            numStr=i;
+            str=minSum;
+            }
+        }
+    }
+    Console.WriteLine($"Строка с наименьшей суммой элементов: {numStr}");
+}
+strMin(mas56);
+
+
+// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
